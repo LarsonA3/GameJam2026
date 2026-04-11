0aabbe292cfaa1133546f62e8c3d2748c7951be2
outcastedplayer <wjwoodson03@gmail.com>
2026-04-11 15:47:00 -0500

    ready to texture room 3

--- GameJam2026Team10/Assets/Scripts/RoomThree/RoomThreeToFourTeleport.cs
+++ GameJam2026Team10/Assets/Scripts/RoomThree/RoomThreeToFourTeleport.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Required for scene management
 
-public class RoomThreeToFourTeleport : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Function to load a scene by its name
+    public void LoadByName(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // Only trigger if the player enters
+        {
+            SceneManager.LoadScene("RoomFour");
+        }
     }
 }
