6c3014661bf30a904648962dcf41dda1299d861d
LarsonA3 <glitchy721@gmail.com>
2026-04-10 20:49:10 -0500

    Finished Title screen
    Made a barebones but working title screen with sound effects, background music, new fonts, play/controls/quit button with two different menus
    tested in build, preparing to merge.

--- GameJam2026Team10/Assets/Scripts/TitleScreen/PlayButton.cs
+++ GameJam2026Team10/Assets/Scripts/TitleScreen/PlayButton.cs
@@ -1,32 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
+    private AudioSource hoverSound;
+    private AudioSource pressedSound;
+    private Image img;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hoverSound = GetComponent<AudioSource>();
+        pressedSound = this.transform.Find("Text (TMP)").GetComponent<AudioSource>();
+        img = gameObject.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 
     public void OnClick() {
+        //sound plays button
+        print("play button clicked");
+        pressedSound.Play();
         StartCoroutine(PlaySequence());
     }
 
 
     private IEnumerator PlaySequence()
     {
+        print("play seq started");
         //screen fade to black
         //play voicelines and overlay text
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(1f);
 
         //bright flash of light and load scene
         SceneManager.LoadScene("RoomOne");
